b0a9e8570d13354a61a30b1aef09c6ba34af444f
Adria Casajus <adria@ecm.ub.es>
2007-11-08 19:41:12 +0000

    Moved about to info added new info menu schema
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@5161 4525493e-7705-40b1-a816-d608a930855b

---
+++ dirac/controllers/info/general.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+from dirac.lib.base import *
+
+log = logging.getLogger(__name__)
+
+class GeneralController(BaseController):
+
+  def index(self):
+    return render( "/info/diracOverview.mako" )
+
+  def diracOverview(self):
+    return render( "/info/diracOverview.mako" )
+
+  def diracWhitepaper(self):
+    return render( "/info/diracOverview.mako" )
+
+  def teamPeople(self):
+    return render( "/info/diracOverview.mako" )
+
+  def teamRestaurants(self):
+    return render( "/info/diracOverview.mako" )
+
+  def teamPubs(self):
+    return render( "/info/diracOverview.mako" )
+
+  def download(self):
+    return render( "/info/diracOverview.mako" )
+
+  def logos(self):
+    c.imagePath = "%s/public/images/logos/" % os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    return render( "/info/logos.mako" )
+    
+  def haiku(self):
+    return render( "/info/haiku.mako" )
+
+  def meetings(self):
+    return render( "/info/meetings.mako" )
