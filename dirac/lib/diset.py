3c7ebb6cf1fd58c688f49b3a025ad76600f31ecf
Adria Casajus <adria@ecm.ub.es>
2007-12-19 17:52:14 +0000

    Modified to suit the new way of specified delegatedDN's
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@5796 4525493e-7705-40b1-a816-d608a930855b

--- dirac/lib/diset.py
+++ dirac/lib/diset.py
@@ -1,8 +1,20 @@
 
-import dirac.lib.sessionManager as sessionManager 
+import dirac.lib.sessionManager as sessionManager
 from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.DISET.TransferClient import TransferClient
+
+def __prepareArgs( kwargs ):
+	if str( sessionManager.getUserDN() ):
+		kwargs[ 'delegatedGroup' ] =  str( sessionManager.getSelectedGroup() )
+		kwargs[ 'delegatedDN' ] = str( sessionManager.getUserDN() )
+	kwargs[ 'useCertificates' ] = True
+	kwargs[ 'setup' ] = sessionManager.getSelectedSetup()
+	return kwargs
 
 def getRPCClient( *args, **kwargs ):
-	kwargs[ 'groupToUse' ] = ( str( sessionManager.getUserDN() ), str( sessionManager.getSelectedGroup() ) )
-	kwargs[ 'useCertificates' ] = True
+	kwargs = __prepareArgs( kwargs )
 	return RPCClient( *args, **kwargs )
+
+def getTransferClient( *args, **kwargs ):
+	kwargs = __prepareArgs( kwargs )
+	return TransferClient( *args, **kwargs )
