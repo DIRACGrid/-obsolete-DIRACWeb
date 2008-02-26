08762f6661d05d863c9c70eceadbdd80ee028386
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2008-02-26 07:48:52 +0000

    Proper version of controller adopted for apache
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@6871 4525493e-7705-40b1-a816-d608a930855b

--- dirac/controllers/jobs/ProductionMonitor.py
+++ dirac/controllers/jobs/ProductionMonitor.py
@@ -1,5 +1,4 @@
 import logging, string
-#import math
 from time import time, gmtime, strftime
 
 from dirac.lib.base import *
@@ -8,10 +7,6 @@ from dirac.lib.credentials import authorizeAction
 from dirac.lib.sessionManager import *
 from DIRAC import gConfig
 log = logging.getLogger(__name__)
-
-##RPC = getRPCClient("ProductionManagement/ProductionRepository")
-RPC = getRPCClient("dips://volhcb03.cern.ch:9131/ProductionManagement/ProductionManager")
-##RPC = getRPCClient("ProductionManagement/ProductionManager")
 
 result = gConfig.getSections("/Users")
 if result["OK"]:
@@ -24,23 +19,18 @@ else:
 
 class ProductionmonitorController(BaseController):
 ################################################################################
-  def index(self):
-    print "INIT"
-    result = RPC.getListProductions()
-    print result
+  def display(self):
+    RPC = getRPCClient("ProductionManagement/ProductionManager")
+    result = RPC.getProductionSummary()
     if result["OK"]:
       prods = result["Value"]
-      print prods
+      print "PRODS:",prods
       if len(prods) < 1:
         return "There is no production available"
       else:
         valueList = []
-        for i in prods:
-          i["TransformationID"] = int(i["TransformationID"])
-          i["TransformationID"] = str(i["TransformationID"]).zfill(8)
-          i["JobsTotal"] = 0#int(i["JobsTotal"])
-          i["JobsSubmitted"] = 0#int(i["JobsSubmitted"])
-          i["LastSubmittedJob"] = 0#int(i["LastSubmittedJob"])
+        for keys,i in prods.items():
+          id = str(int(keys)).zfill(8)
           DN = i["AuthorDN"]
           if len(DN) > 0:
             if dndb.has_key(DN):
@@ -49,22 +39,45 @@ class ProductionmonitorController(BaseController):
               i["AuthorDN"] = "Owner Unknown" # Zdes' nado probovat' esche raz
           else:
             i["AuthorDN"] = "Owner Unknown"
-          valueList.append([i["TransformationID"],i["TransformationName"],i["Status"],i["AuthorDN"],i["JobsTotal"],i["JobsSubmitted"],i["LastSubmittedJob"],i["AgentType"],i["Description"]])
+          jobStat = i["JobStats"]
+          created = jobStat["Created"]
+          submited = jobStat["Submitted"]
+          wait = jobStat["Waiting"]
+          running = jobStat["Running"]
+          done = jobStat["Done"]
+          failed = jobStat["Failed"]
+          valueList.append([id,i["TransformationName"],i["Status"],i["AuthorDN"],created,submited,wait,running,done,failed,i["AgentType"],i["Description"],i["CreationDate"]])
         c.listResult = valueList
+        print "OVERALLRESULT",c.listResult
         return render("/jobs/ProductionMonitor.mako")
     else:
       print "+++ E:",result["Message"]
       return "Failed during RPC call. %s" % result["Message"]
 ################################################################################
+  @jsonify
   def action(self):
     pagestart = time()
     if request.params.has_key("getInfo") and len(request.params["getInfo"]) > 0:
       id = str(request.params["getInfo"])
       mode = str(request.params["mode"])
       return self.__getInfo(id,mode)
+    elif request.params.has_key("Refresh") and len(request.params["Refresh"]) > 0:
+      id = str(request.params["Refresh"])
+      if id == "true":
+        return self.__getProd()
+      else:
+        c.error = "Invalid request %s" % id
+        print "+++ E:",c.error
+        return c.error
+    elif request.params.has_key("startProd") and len(request.params["startProd"]) > 0:
+      id = str(request.params["startProd"])
+      return self.__actProduction(id,"start")
+    elif request.params.has_key("stopProd") and len(request.params["stopProd"]) > 0:
+      id = str(request.params["stopProd"])
+      return self.__actProduction(id,"stop")
     elif request.params.has_key("delProd") and len(request.params["delProd"]) > 0:
       id = str(request.params["delProd"])
-      return self.__deleteProduction(id)
+      return self.__actProduction(id,"del")
     elif request.params.has_key("LoggingInfo") and len(request.params["LoggingInfo"]) > 0:
       id = int(request.params["LoggingInfo"])
       return self.__getLoggingInfo(id)
@@ -77,11 +90,47 @@ class ProductionmonitorController(BaseController):
       id = [int(i) for i in id ]
       return self.__delJobs(id)
     else:
-      c.error = "Failed to get DIRAC job ID"
+      c.error = "Failed to get ID"
       print "+++ E:",c.error
       return c.error
 ################################################################################
+  def __getProd(self):
+    RPC = getRPCClient("ProductionManagement/ProductionManager")
+    result = RPC.getProductionSummary()
+    if result["OK"]:
+      prods = result["Value"]
+      print "PRODS:",prods
+      if len(prods) < 1:
+        return "There is no production available"
+      else:
+        valueList = []
+        for keys,i in prods.items():
+          id = str(int(keys)).zfill(8)
+          DN = i["AuthorDN"]
+          if len(DN) > 0:
+            if dndb.has_key(DN):
+              i["AuthorDN"] = dndb[DN]
+            else:
+              i["AuthorDN"] = "Owner Unknown" # Zdes' nado probovat' esche raz
+          else:
+            i["AuthorDN"] = "Owner Unknown"
+          jobStat = i["JobStats"]
+          created = jobStat["Created"]
+          submited = jobStat["Submitted"]
+          wait = jobStat["Waiting"]
+          running = jobStat["Running"]
+          done = jobStat["Done"]
+          failed = jobStat["Failed"]
+          valueList.append([id,i["TransformationName"],i["Status"],i["AuthorDN"],created,submited,wait,running,done,failed,i["AgentType"],i["Description"],i["CreationDate"]])
+        c.listResult = valueList
+        print "OVERALLRESULT",c.listResult
+        return c.listResult
+    else:
+      print "+++ E:",result["Message"]
+      return "Failed during RPC call. %s" % result["Message"]
+################################################################################
   def __getInfo(self,id,mode):
+    RPC = getRPCClient("ProductionManagement/ProductionRepository")
     if mode == "Comment":
       print "Name or ID:",id
       result = RPC.getProductionFullDescription(id)
@@ -102,8 +151,15 @@ class ProductionmonitorController(BaseController):
       print "+++ E:",result["Message"]
       return "Failed during RPC call. %s" % result["Message"]
 ################################################################################
-  def __deleteProduction(self,id):
-    result = RPC.deleteProductionID(id)
+  def __actProduction(self,prodid,cmd):
+    id = int(prodid)
+    RPC = getRPCClient("ProductionManagement/ProductionManager")
+    if cmd == "del":
+      result = RPC.deleteProductionID(id)
+    elif cmd == "start":
+      result = RPC.setProductionStatus(id,"Active")
+    elif cmd == "stop":
+      result = RPC.setProductionStatus(id,"Stopped")
     print result
     if result["OK"]:
       return 0
