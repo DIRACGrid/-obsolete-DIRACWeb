c9a75fcffb7b82a43c76c5added69f6938d174e6
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2008-05-22 12:14:35 +0000

    StorageSpaceMonitor
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@9121 4525493e-7705-40b1-a816-d608a930855b

--- dirac/public/javascripts/web/External.js
+++ dirac/public/javascripts/web/External.js
@@ -9,7 +9,13 @@ function initJobMonitor(reponseSelect){
 function renderData(site){
   var leftBar = new Ext.Panel();
   leftBar.hide();
-  site = 'https://lemonweb.cern.ch/lemon-web/info.php?entity=' + site + '&detailed=yes';
+  if(site){
+    if((site=='volhcb01')||(site=='volhcb02')||(site=='volhcb03')||(site=='volhcb04')||(site=='volhcb05')||(site=='volhcb06')||(site=='volhcb07')){
+      site = 'https://lemonweb.cern.ch/lemon-web/info.php?entity=' + site + '&detailed=yes';
+    }else if((site=='LHCbDST')||(site=='LHCbRDST')||(site=='LHCbMDST')||(site=='LHCbUSER')||(site=='LHCbFAILOVER')||(site=='LHCbRAW')){
+      site = 'https://santinel.web.cern.ch/santinel/sls/storage_space/' + site + '.html'
+    }
+  }
   var html = '<iframe id="www_frame" src =' + site + '></iframe>';
   var mainContent = new Ext.Panel({border:0,autoScroll:false,html:html,region:'center'})
   mainContent.on('resize',function(){
