f0f2d3b9281aea12611a450afa0e628636cafa03
Adria Casajus <adria@ecm.ub.es>
2008-06-17 11:07:22 +0000

    html raw page
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@9798 4525493e-7705-40b1-a816-d608a930855b

---
+++ dirac/public/javascripts/htmlPage.js
@@ -0,0 +1,27 @@
+gMainPanel = false;
+
+function initHTML( htmlID ){
+  Ext.onReady(function(){
+    renderPage( htmlID );
+  });
+}
+
+function renderPage( htmlID )
+{
+  var rawEl = Ext.get( htmlID );
+  if( !rawEl )
+  {
+  	alert( "Can't get element " + htmlID );
+  }
+
+  gMainPanel = new Ext.Panel( {
+  							region : 'center',
+  							enableTabScroll : true,
+  							defaults: { autoScroll:true },
+  							contentEl : htmlID,
+//  							items : [ rawEl ],
+  							 } );
+  //rawEl.remove();
+
+  renderInMainViewport( [ gMainPanel ] );
+}
