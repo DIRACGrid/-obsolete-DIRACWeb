55f077e83aea355710e2d13c8cf1a9d70f35df20
Adria Casajus <adria@ecm.ub.es>
2008-07-10 11:11:34 +0000

    bugfix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@10501 4525493e-7705-40b1-a816-d608a930855b

--- dirac/public/javascripts/systems/configuration/csTree.js
+++ dirac/public/javascripts/systems/configuration/csTree.js
@@ -210,12 +210,13 @@ function inPlaceOptionValueChange( node, clickEvent )
 {
 	node.un( "beforeclick", inPlaceOptionValueChange );
 	var editId = "opedit:" + node.id;
-	node.setText( node.attributes.csName + " = <input id='"+editId+"' type='text' value='" + node.attributes.csValue +"'/>" );
+	node.setText( node.attributes.csName + " = <input id='"+editId+"' type='text' size='100' value='" + node.attributes.csValue +"'/>" );
 	var editObj = Ext.get( editId );
 	editId.treeNode = node;
 	Ext.EventManager.on( editObj, "keypress", cbNodeEditKeyPressed, editObj, node );
 	Ext.EventManager.on( editObj, "blur", cbNodeEditBlur, editObj, node );
 	editObj.focus();
+	editObj.select();
 	return true;
 }
 /*
@@ -385,7 +386,7 @@ function serverCopyNode( ajaxResponse, reqArguments )
 						csName : newName,
 						csComment : node.attributes.csComment,
 					};
-	if( node.isLeaf )
+	if( node.isLeaf() )
 	{
 		newCfg.leaf = true;
 		newCfg.csValue = node.attributes.csValue;
@@ -421,7 +422,7 @@ function serverRenameNode( ajaxResponse, reqArguments )
 	var newName = reqArguments.params.newName;
 	var node = reqArguments.node;
 	node.attributes.csName = newName;
-	if( node.isLeaf )
+	if( node.isLeaf() )
 	{
 		configureLeafNode( node );
 	}
