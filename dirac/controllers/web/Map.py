fe7ba205c0d77851bc548614d1e870e7e41e85c0
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2008-07-21 13:52:31 +0000

    initiall release of the sitemap controller, cache add method seems doesn't work
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@10811 4525493e-7705-40b1-a816-d608a930855b

---
+++ dirac/controllers/web/Map.py
@@ -0,0 +1,81 @@
+import logging, os, tempfile
+from time import time, gmtime, strftime
+
+from dirac.lib.base import *
+from dirac.lib.diset import getRPCClient, getTransferClient
+from dirac.lib.credentials import authorizeAction
+from dirac.lib.sessionManager import *
+from DIRAC.Core.Utilities.DictCache import DictCache
+from DIRAC import gLogger
+from DIRAC import S_ERROR, S_OK
+
+log = logging.getLogger(__name__)
+
+#cache = DictCache()
+
+class MapController(BaseController):
+  __imgCache = DictCache()
+  def display(self):
+    return render("web/Map.mako")
+################################################################################
+  def getKML(self):
+    pagestart = time()
+    transferClient = getTransferClient('Monitoring/SiteMapping')
+    if "name" not in request.params:
+      c.result = "file name is absent"
+      return c.result
+    name = request.params["name"]
+#    if name.find( ".kml" ) < -1:
+#      c.result = "Not a valid file"
+#      return c.result
+    gLogger.info("request for the file:", name)
+    data = self.__imgCache.get(name)
+    gLogger.info("data from cache:", data)
+    if not data:
+      tmpFile = tempfile.TemporaryFile()
+      result = transferClient.receiveFile(tmpFile, {'Type':'File', 'Data':name})
+      teststring = "operation result for request file " + name + ": "
+      gLogger.info(teststring, result)
+      if not result["OK"]:
+        c.result = result["Message"]
+        return c.result
+      tmpFile.seek( 0 )
+      data = tmpFile.read()
+      self.__imgCache.add(name, 600, data)
+#      gLogger.info("CACHE after add:", self.__imgCache.getKeys(100))
+    else:
+      gLogger.info("this file taken from cache:", name)
+    response.headers['Content-type'] = 'text/xml; charset=utf-8'
+    gLogger.info("CACHE:", self.__imgCache.getKeys(600))
+    return data
+################################################################################
+  def getImg(self):
+    pagestart = time()
+    transferClient = getTransferClient("Monitoring/SiteMapping")
+    if "name" not in request.params:
+      c.result = "file name is absent"
+      return c.result
+    name = request.params["name"]
+#    if name.find( ".png" ) < -1:
+#      c.result = "Not a valid image file"
+#      return c.result
+    gLogger.info("request for the file:", name)
+    data = self.__imgCache.get(name)
+    if not data:
+      tmpFile = tempfile.TemporaryFile()
+      result = transferClient.receiveFile(tmpFile, {'Type':'File', 'Data':name})
+      teststring = "operation result for request file " + name + ": "
+      gLogger.info(teststring, result)
+      if not result["OK"]:
+        c.result = result["Message"]
+        return c.result
+      tmpFile.seek( 0 )
+      data = tmpFile.read()
+      self.__imgCache.add(name, 600, data)
+    else:
+      gLogger.info("this file taken from cache:", name)
+    response.headers['Content-type'] = 'image/png'
+    response.headers['Content-Disposition'] = 'attachment; filename="%s"' % name
+    response.headers['Content-Length'] = len(data)
+    response.headers['Content-Transfer-Encoding'] = 'Binary'
+    return data
