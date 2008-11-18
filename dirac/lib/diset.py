27e9a48ec86d5d5af1602c8124446fa4d2943b28
Adria Casajus <adria@ecm.ub.es>
2008-11-18 15:58:43 +0000

    Added client for the UserProfileManager
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@13087 4525493e-7705-40b1-a816-d608a930855b

--- dirac/lib/diset.py
+++ dirac/lib/diset.py
@@ -1,7 +1,9 @@
 
+from pylons import request
 import dirac.lib.credentials as credentials
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.Core.DISET.TransferClient import TransferClient
+from DIRAC.FrameworkSystem.Client.UserProfileClient import UserProfileClient
 
 def __prepareArgs( kwargs ):
   if credentials.getUserDN():
@@ -18,3 +20,8 @@ def getRPCClient( *args, **kwargs ):
 def getTransferClient( *args, **kwargs ):
   kwargs = __prepareArgs( kwargs )
   return TransferClient( *args, **kwargs )
+
+def getUserProfileClient():
+  routingDict = request.environ[ 'wsgiorg.routing_args' ][1]
+  action = str( "%s/%s" % ( routingDict[ 'controller' ], routingDict[ 'action' ] ) )
+  return UserProfileClient( action, rpcClient = getRPCClient( "Framework/UserProfileManager", timeout = 600 ) )
