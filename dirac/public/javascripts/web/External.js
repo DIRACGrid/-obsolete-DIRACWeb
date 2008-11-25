c8edb4ce62c5109f87a22457f0253c30bd170f1a
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2008-11-25 17:43:11 +0000

    SAM test support
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@13181 4525493e-7705-40b1-a816-d608a930855b

--- dirac/public/javascripts/web/External.js
+++ dirac/public/javascripts/web/External.js
@@ -10,10 +10,18 @@ function renderData(site){
   var leftBar = new Ext.Panel();
   leftBar.hide();
   if(site){
-    if((site=='volhcb01')||(site=='volhcb02')||(site=='volhcb03')||(site=='volhcb04')||(site=='volhcb05')||(site=='volhcb06')||(site=='volhcb07')){
+    if((site=='volhcb01')||(site=='volhcb02')||(site=='volhcb03')||(site=='volhcb04')||(site=='volhcb05')||(site=='volhcb06')||(site=='volhcb07')||(site=='volhcb08')||(site=='volhcb09')||(site=='volhcb10')||(site=='volhcb11')){
       site = 'https://lemonweb.cern.ch/lemon-web/info.php?entity=' + site + '&detailed=yes';
     }else if((site=='LHCbDST')||(site=='LHCbRDST')||(site=='LHCbMDST')||(site=='LHCbUSER')||(site=='LHCbFAILOVER')||(site=='LHCbRAW')){
-      site = 'https://santinel.web.cern.ch/santinel/sls/storage_space/' + site + '.html'
+      site = 'https://santinel.web.cern.ch/santinel/sls/storage_space/' + site + '.html';
+    }else if((site == 'DIRAC-VOBOX')||(site == 'T1VOBOX')){
+      site = 'https://sls.cern.ch/sls/service.php?id=' + site;
+    }else if(site=='space token'){
+      site = 'http://wn3.epcc.ed.ac.uk/srm/xml/srm_token_space';
+    }else if(site == 'LHCb-SAM'){
+      site = 'http://dashb-lhcb-sam.cern.ch/dashboard/request.py/latestresultssmry?siteSelect3=501&serviceTypeSelect3=vo&sites=CERN-PROD&sites=FZK-LCG2&sites=IN2P3-CC&sites=INFN-T1&sites=NIKHEF-ELPROD&sites=RAL-LCG2&sites=pic&services=CE&tests=37535&tests=398&tests=404&tests=405&tests=406&tests=403&tests=37415&tests=407&tests=399&exitStatus=all&table=true';
+    }else if(site=='calendar'){
+      site = 'http://romanov.web.cern.ch/romanov/Downtime/calendar.html';
     }
   }
   var html = '<iframe id="www_frame" src =' + site + '></iframe>';
