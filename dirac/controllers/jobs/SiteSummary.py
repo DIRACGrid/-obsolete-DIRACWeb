6cd5b0110f9ac79a977bca18f54ecf99c539108a
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2009-04-05 21:40:18 +0000

    Sort by countries is introduced
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@14911 4525493e-7705-40b1-a816-d608a930855b

--- dirac/controllers/jobs/SiteSummary.py
+++ dirac/controllers/jobs/SiteSummary.py
@@ -47,27 +47,22 @@ class SitesummaryController(BaseController):
               jobs = result["Records"]
               head = result["ParameterNames"]
               headLength = len(head)
+              countryCode = self.__countries()
               for i in jobs:
                 tmp = {}
                 for j in range(0,headLength):
                   tmp[head[j]] = i[j]
+                if countryCode.has_key(i[2]):
+                  tmp["FullCountry"] = countryCode[i[2]]
+                else:
+                  tmp["FullCountry"] = "Unknown"
                 c.result.append(tmp)
               total = result["TotalRecords"]
-              plots = 'test.png'
-              if result.has_key("Plots"):
-                imgCache.add("test.png",600,result["Plots"])
-                res = imgCache.exists(name)
-                gLogger.info("exists? ", res)
-#                tmpFile = tempfile.NamedTemporaryFile(suffix='.png')
-#                tmpFile.write(result["Plots"])
-#                tmpFile.close()
-#                plots = tmpFile.name
-#                gLogger.info("\033[0;31mSITESUMMARY INDEX REQUEST:\033[0m",tempfile.gettempdir())
               if result.has_key("Extras"):
                 extra = result["Extras"]
-                c.result = {"success":"true","result":c.result,"total":total,"extra":extra,"plots":plots}
+                c.result = {"success":"true","result":c.result,"total":total,"extra":extra}
               else:
-                c.result = {"success":"true","result":c.result,"total":total,"plots":plots}
+                c.result = {"success":"true","result":c.result,"total":total}
             else:
               c.result = {"success":"false","result":"","error":"There are no data to display"}
           else:
@@ -78,37 +73,69 @@ class SitesummaryController(BaseController):
         c.result = {"success":"false","result":"","error":"There were no data matching your selection"}
     else:
       c.result = {"success":"false","error":result["Message"]}
-    gLogger.info("\033[0;31mJOB SUBMIT REQUEST:\033[0m %s" % (time() - pagestart))
+    gLogger.info("\033[0;31m SITESUMMARY INDEX REQUEST: \033[0m %s" % (time() - pagestart))
     return c.result
 ###############################################################################
   def __getSelectionData(self):
     callback = {}
-    RPC = getRPCClient("ProductionManagement/ProductionManager")
-    result = RPC.getProductionSummary()
+###
+    RPC = getRPCClient("WorkloadManagement/WMSAdministrator")
+    result = RPC.getSiteSummarySelectors()
+    gLogger.info("\033[0;31m ++++++: \033[0m %s" % result)
     if result["OK"]:
-      prod = []
-      prods = result["Value"]
-      if len(prods)>0:
-        for keys,i in prods.items():
-          id = str(int(keys)).zfill(8)
-          prod.append([str(id)])
+      result = result["Value"]
+      if result.has_key("Status") and len(result["Status"]) > 0:
+        status = []
+        status.append([str("All")])
+        for i in result["Status"]:
+          status.append([str(i)])
       else:
-        prod = "Nothing to display"
-    else:
-      prod = "Error during RPC call"
-    callback["prod"] = prod
-    RPC = getRPCClient("WorkloadManagement/JobMonitoring")
-    result = RPC.getSites()
-    if result["OK"]:
-      site = []
-      if len(result["Value"])>0:
-        for i in result["Value"]:
+        status = [["Nothing to display"]]
+      callback["status"] = status
+      if result.has_key("GridType") and len(result["GridType"]) > 0:
+        gridtype = []
+        gridtype.append([str("All")])
+        for i in result["GridType"]:
+          gridtype.append([str(i)])
+      else:
+        gridtype = [["Nothing to display"]]
+      callback["gridtype"] = gridtype
+      if result.has_key("MaskStatus") and len(result["MaskStatus"]) > 0:
+        maskstatus = []
+        maskstatus.append([str("All")])
+        for i in result["MaskStatus"]:
+          maskstatus.append([str(i)])
+      else:
+        maskstatus = [["Nothing to display"]]
+      callback["maskstatus"] = maskstatus
+      if result.has_key("Site") and len(result["Site"]) > 0:
+        site = []
+        site.append([str("All")])
+        for i in result["Site"]:
           site.append([str(i)])
       else:
-        site = "Nothing to display"
+        site = [["Nothing to display"]]
+      callback["site"] = site
+      if result.has_key("Country") and len(result["Country"]) > 0:
+        country = []
+        country.append([str("All"),str("All")])
+        countryCode = self.__countries()
+        for i in result["Country"]:
+          if countryCode.has_key(i):
+            j = countryCode[i]
+          else:
+            j = "Unknown"
+          country.append([str(j),str(i)])
+      else:
+        country = [["Nothing to display"]]
+      callback["country"] = country
     else:
-      site = "Error during RPC call"
-    callback["site"] = site
+      callback["status"] = [["Error during RPC call"]]
+      callback["gridtype"] = [["Error during RPC call"]]
+      callback["maskstatus"] = [["Error during RPC call"]]
+      callback["site"] = [["Error during RPC call"]]
+      callback["country"] = [["Error during RPC call"]]
+###
     return callback
 ################################################################################
   def __request(self):
@@ -117,32 +144,40 @@ class SitesummaryController(BaseController):
     if request.params.has_key("id") and len(request.params["id"]) > 0:
       pageNumber = 0
       req["JobID"] = str(request.params["id"])
+    elif request.params.has_key("expand") and len(request.params["expand"]) > 0:
+      global globalSort
+      global numberOfJobs
+      globalSort = [["GridSite","ASC"]]
+      numberOfJobs = 500
+      pageNumber = 0
+      req["ExpandSite"] = str(request.params["expand"])
     else:
       global numberOfJobs
       global globalSort
-      if request.params.has_key("limit") and len(request.params["limit"]) > 0:
-        if request.params.has_key("start") and len(request.params["start"]) > 0:
-          numberOfJobs = int(request.params["limit"])
-          pageNumber = int(request.params["start"])
-        else:
-          pageNumber = 0
-      else:
-        numberOfJobs = 25
-      if request.params.has_key("prod") and len(request.params["prod"]) > 0:
-        if str(request.params["prod"]) != "All":
-          req["JobGroup"] = str(request.params["prod"]).split('::: ')
+      pageNumber = 0
+      numberOfJobs = 500
+      if request.params.has_key("country") and len(request.params["country"]) > 0:
+        if str(request.params["country"]) != "All":
+          code = self.__reverseCountry()
+          tmpValue = str(request.params["country"]).split('::: ')
+          newValue = []
+          for i in tmpValue:
+            if code.has_key(i):
+              newValue.append(code[i])
+          req["Country"] = newValue
+#          req["Country"] = str(request.params["country"]).split('::: ')
       if request.params.has_key("site") and len(request.params["site"]) > 0:
         if str(request.params["site"]) != "All":
-          req["GridSite"] = str(request.params["site"]).split('::: ')
-      if request.params.has_key("stat") and len(request.params["stat"]) > 0:
-        if str(request.params["stat"]) != "All":
-          req["Status"] = str(request.params["stat"]).split('::: ')
-      if request.params.has_key("minorstat") and len(request.params["minorstat"]) > 0:
-        if str(request.params["minorstat"]) != "All":
-          req["MinorStatus"] = str(request.params["minorstat"]).split('::: ')
-      if request.params.has_key("app") and len(request.params["app"]) > 0:
-        if str(request.params["app"]) != "All":
-          req["ApplicationStatus"] = str(request.params["app"]).split('::: ')
+          req["Site"] = str(request.params["site"]).split('::: ')
+      if request.params.has_key("status") and len(request.params["status"]) > 0:
+        if str(request.params["status"]) != "All":
+          req["Status"] = str(request.params["status"]).split('::: ')
+      if request.params.has_key("maskstatus") and len(request.params["maskstatus"]) > 0:
+        if str(request.params["maskstatus"]) != "All":
+          req["MaskStatus"] = str(request.params["maskstatus"]).split('::: ')
+      if request.params.has_key("gridtype") and len(request.params["gridtype"]) > 0:
+        if str(request.params["gridtype"]) != "All":
+          req["GridType"] = str(request.params["gridtype"]).split('::: ')
       else:
         if request.params.has_key("owner") and len(request.params["owner"]) > 0:
           if str(request.params["owner"]) != "All":
@@ -174,4 +209,256 @@ class SitesummaryController(BaseController):
 #    response.headers['Content-Length'] = len(data)
 #    response.headers['Content-Transfer-Encoding'] = 'Binary'
     return data
-
+################################################################################
+  def __reverseCountry(self):
+    result = self.__countries()
+    name = {}
+    for code, country in result.items():
+      name[country] = code
+    return name
+################################################################################
+  def __countries(self):
+    countries = {
+    "af": "Afghanistan",
+    "al": "Albania",
+    "dz": "Algeria",
+    "as": "American Samoa",
+    "ad": "Andorra",
+    "ao": "Angola",
+    "ai": "Anguilla",
+    "aq": "Antarctica",
+    "ag": "Antigua and Barbuda",
+    "ar": "Argentina",
+    "am": "Armenia",
+    "aw": "Aruba",
+    "au": "Australia",
+    "at": "Austria",
+    "az": "Azerbaijan",
+    "bs": "Bahamas",
+    "bh": "Bahrain",
+    "bd": "Bangladesh",
+    "bb": "Barbados",
+    "by": "Belarus",
+    "be": "Belgium",
+    "bz": "Belize",
+    "bj": "Benin",
+    "bm": "Bermuda",
+    "bt": "Bhutan",
+    "bo": "Bolivia",
+    "ba": "Bosnia and Herzegowina",
+    "bw": "Botswana",
+    "bv": "Bouvet Island",
+    "br": "Brazil",
+    "io": "British Indian Ocean Territory",
+    "bn": "Brunei Darussalam",
+    "bg": "Bulgaria",
+    "bf": "Burkina Faso",
+    "bi": "Burundi",
+    "kh": "Cambodia",
+    "cm": "Cameroon",
+    "ca": "Canada",
+    "cv": "Cape Verde",
+    "ky": "Cayman Islands",
+    "cf": "Central African Republic",
+    "td": "Chad",
+    "cl": "Chile",
+    "cn": "China",
+    "cx": "Christmas Island",
+    "cc": "Cocos Islands",
+    "co": "Colombia",
+    "km": "Comoros",
+    "cg": "Congo",
+    "cd": "Congo",
+    "ck": "Cook Islands",
+    "cr": "Costa Rica",
+    "ci": "Cote D'Ivoire",
+    "hr": "Croatia",
+    "cu": "Cuba",
+    "cy": "Cyprus",
+    "cz": "Czech Republic",
+    "dk": "Denmark",
+    "dj": "Djibouti",
+    "dm": "Dominica",
+    "do": "Dominican Republic",
+    "tp": "East Timor",
+    "ec": "Ecuador",
+    "eg": "Egypt",
+    "sv": "El Salvador",
+    "gq": "Equatorial Guinea",
+    "er": "Eritrea",
+    "ee": "Estonia",
+    "et": "Ethiopia",
+    "fk": "Falkland Islands",
+    "fo": "Faroe Islands",
+    "fj": "Fiji",
+    "fi": "Finland",
+    "fr": "France",
+    "fx": "France, metropolitan",
+    "gf": "French Guiana",
+    "pf": "French Polynesia",
+    "tf": "French Southern Territories",
+    "ga": "Gabon",
+    "gm": "Gambia",
+    "ge": "Georgia",
+    "de": "Germany",
+    "gh": "Ghana",
+    "gi": "Gibraltar",
+    "gr": "Greece",
+    "gl": "Greenland",
+    "gd": "Grenada",
+    "gp": "Guadeloupe",
+    "gu": "Guam",
+    "gt": "Guatemala",
+    "gn": "Guinea",
+    "gw": "Guinea-Bissau",
+    "gy": "Guyana",
+    "ht": "Haiti",
+    "hm": "Heard and Mc Donald Islands",
+    "va": "Vatican City",
+    "hn": "Honduras",
+    "hk": "Hong Kong",
+    "hu": "Hungary",
+    "is": "Iceland",
+    "in": "India",
+    "id": "Indonesia",
+    "ir": "Iran",
+    "iq": "Iraq",
+    "ie": "Ireland",
+    "il": "Israel",
+    "it": "Italy",
+    "jm": "Jamaica",
+    "jp": "Japan",
+    "jo": "Jordan",
+    "kz": "Kazakhstan",
+    "ke": "Kenya",
+    "ki": "Kiribati",
+    "kp": "Korea",
+    "kr": "Korea",
+    "kw": "Kuwait",
+    "kg": "Kyrgyzstan",
+    "la": "Lao",
+    "lv": "Latvia",
+    "lb": "Lebanon",
+    "ls": "Lesotho",
+    "lr": "Liberia",
+    "ly": "Libyan",
+    "li": "Liechtenstein",
+    "lt": "Lithuania",
+    "lu": "Luxembourg",
+    "mo": "Macau",
+    "mk": "Macedonia",
+    "mg": "Madagascar",
+    "mw": "Malawi",
+    "my": "Malaysia",
+    "mv": "Maldives",
+    "ml": "Mali",
+    "mt": "Malta",
+    "mh": "Marshall Islands",
+    "mq": "Martinique",
+    "mr": "Mauritania",
+    "mu": "Mauritius",
+    "yt": "Mayotte",
+    "mx": "Mexico",
+    "fm": "Micronesia",
+    "md": "Moldova",
+    "mc": "Monaco",
+    "mn": "Mongolia",
+    "ms": "Montserrat",
+    "ma": "Morocco",
+    "mz": "Mozambique",
+    "mm": "Myanmar",
+    "na": "Namibia",
+    "nr": "Nauru",
+    "np": "Nepal",
+    "nl": "Netherlands",
+    "an": "Netherlands Antilles",
+    "nc": "New Caledonia",
+    "nz": "New Zealand",
+    "ni": "Nicaragua",
+    "ne": "Niger",
+    "ng": "Nigeria",
+    "nu": "Niue",
+    "nf": "Norfolk Island",
+    "mp": "Northern Mariana Islands",
+    "no": "Norway",
+    "om": "Oman",
+    "pk": "Pakistan",
+    "pw": "Palau",
+    "pa": "Panama",
+    "pg": "Papua New Guinea",
+    "py": "Paraguay",
+    "pe": "Peru",
+    "ph": "Philippines",
+    "pn": "Pitcairn",
+    "pl": "Poland",
+    "pt": "Portugal",
+    "pr": "Puerto Rico",
+    "qa": "Qatar",
+    "re": "Reunion",
+    "ro": "Romania",
+    "ru": "Russia",
+    "rw": "Rwanda",
+    "kn": "Saint Kitts and Nevis",
+    "lc": "Saint Lucia",
+    "vc": "Saint Vincent and the Grenadines",
+    "ws": "Samoa",
+    "sm": "San Marino",
+    "st": "Sao Tome and Principe",
+    "sa": "Saudi Arabia",
+    "sn": "Senegal",
+    "sc": "Seychelles",
+    "sl": "Sierra Leone",
+    "sg": "Singapore",
+    "sk": "Slovakia",
+    "si": "Slovenia",
+    "sb": "Solomon Islands",
+    "so": "Somalia",
+    "za": "South Africa",
+    "gs": "South Georgia and the South Sandwich Islands",
+    "es": "Spain",
+    "lk": "Sri Lanka",
+    "sh": "St. Helena",
+    "pm": "St. Pierre and Miquelon",
+    "sd": "Sudan",
+    "sr": "Suriname",
+    "sj": "Svalbard and Jan Mayen Islands",
+    "sz": "Swaziland",
+    "se": "Sweden",
+    "ch": "Switzerland",
+    "sy": "Syrian Arab Republic",
+    "tw": "Taiwan",
+    "tj": "Tajikistan",
+    "tz": "Tanzania",
+    "th": "Thailand",
+    "tg": "Togo",
+    "tk": "Tokelau",
+    "to": "Tonga",
+    "tt": "Trinidad and Tobago",
+    "tn": "Tunisia",
+    "tr": "Turkey",
+    "tm": "Turkmenistan",
+    "tc": "Turks and Caicos Islands",
+    "tv": "Tuvalu",
+    "ug": "Uganda",
+    "ua": "Ukraine",
+    "ae": "United Arab Emirates",
+    "gb": "United Kingdom",
+    "uk": "United Kingdom",
+    "us": "United States",
+    "um": "United States Minor Outlying Islands",
+    "uy": "Uruguay",
+    "uz": "Uzbekistan",
+    "vu": "Vanuatu",
+    "ve": "Venezuela",
+    "vn": "Viet Nam",
+    "vg": "Virgin Islands (British)",
+    "vi": "Virgin Islands (U.S.)",
+    "wf": "Wallis and Futuna Islands",
+    "eh": "Western Sahara",
+    "ye": "Yemen",
+    "yu": "Yugoslavia",
+    "zm": "Zambia",
+    "zw": "Zimbabwe",
+    "su": "Soviet Union"
+    }
+    return countries
