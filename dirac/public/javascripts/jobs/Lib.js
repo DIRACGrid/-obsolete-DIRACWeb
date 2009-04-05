624bb559f405218d017cd3be3e708148791194c0
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2009-04-05 21:49:19 +0000

    5000 entries per table's page is removed
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@14914 4525493e-7705-40b1-a816-d608a930855b

--- dirac/public/javascripts/jobs/Lib.js
+++ dirac/public/javascripts/jobs/Lib.js
@@ -419,7 +419,7 @@ function hideControls(caller){
 function itemsNumber(){
   var store = new Ext.data.SimpleStore({
     fields:['number'],
-    data:[[25],[50],[100],[200],[500],[1000],[5000]]
+    data:[[25],[50],[100],[200],[500],[1000]]
   });
   var value = 25;
   if(dataSelect){
