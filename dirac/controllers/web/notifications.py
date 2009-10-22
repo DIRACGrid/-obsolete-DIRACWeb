b22673f167ffdf55a6ba5b19c41e97072b84b723
Adria Casajus <adria@ecm.ub.es>
2009-10-22 17:36:00 +0000

    Initial commit of web notifications
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@17854 4525493e-7705-40b1-a816-d608a930855b

---
+++ dirac/controllers/web/notifications.py
@@ -0,0 +1,139 @@
+import logging
+import cgi
+import time
+import simplejson
+
+from dirac.lib.base import *
+from pylons.controllers.util import redirect_to
+import dirac.lib.helpers as helpers
+
+from dirac.lib.diset import getRPCClient
+import dirac.lib.credentials as credentials
+
+from DIRAC.FrameworkSystem.Client.NotificationClient import NotificationClient
+from DIRAC import gLogger, S_OK, S_ERROR
+
+from dirac.lib.webBase import defaultRedirect
+
+log = logging.getLogger(__name__)
+
+class NotificationsCache:
+  
+  __cacheTime = 300
+  
+  def __init__(self):
+    self.__userTimestamp = {}
+    self.__stats = {}
+    self.__notifications = {}
+    
+  def getNtfClient( self ):
+    return NotificationClient( getRPCClient )
+    
+  def getStatsForUser( self, forceRefresh = False ):
+    userName = credentials.getUsername()
+    if userName == 'anonymous':
+      return { 'totalNots' : 0 }
+    if not forceRefresh and  userName in self.__userTimestamp:
+      if time.time() - self.__userTimestamp[ userName ] > self.__cacheTime:
+        return self.__stats[ userName ]
+    ntf = self.getNtfClient()
+    result = ntf.getNotifications( {}, [], 0, 0 )
+    if not result[ 'OK' ]:
+      gLogger.error( "Could not retrieve notifications", "for user %s: %s" % ( userName, result[ 'Message' ] ) )
+      return { 'totalNots' : 0 }
+    nots = result[ 'Value' ]
+    self.__notifications[ userName ] = nots
+    total = len( nots[ 'Records' ] )
+    new = []
+    records = []
+    if total:
+      for row in nots[ 'Records' ]:
+        rD = {}
+        for i in range( len( nots[ 'ParameterNames' ] ) ):
+          param = nots[ 'ParameterNames' ][i]
+          if param == 'timestamp':
+            rD[ param ] = row[i].strftime( "%Y/%m/%d %H:%M:%S UTC" )
+          else:
+            rD[ param ] = row[i]
+        records.append( rD ) 
+      new = []
+      for record in records:
+        if not record[ 'seen' ]:
+          new.append( record[ 'id' ] )
+      new.sort()
+    stats = { 'totalNots' : total, 'newNots' : new }
+    self.__stats[ userName ] = stats
+    self.__userTimestamp[ userName ] = time.time()
+    self.__notifications[ userName ] = records
+    return stats
+  
+  def getNotificationsForUser( self ):
+    userName = credentials.getUsername()
+    if userName in self.__notifications:
+      return self.__notifications[ userName ]
+    return {}
+  
+  def markNotificationsAsSeen( self, seen, notifsIds ):
+    userName = credentials.getUsername()
+    ntfCli = self.getNtfClient()
+    if seen:
+      result = ntfCli.markNotificationsAsRead( userName, notifsIds )
+    else:
+      result = ntfCli.markNotificationsAsNotRead( userName, notifsIds )
+    if not result[ 'OK' ]:
+      return S_ERROR( result[ 'Message' ] )
+    self.getStatsForUser( forceRefresh = True )
+    return S_OK()
+  
+  def deleteNotifications( self, notifsIds ):
+    userName = credentials.getUsername()
+    ntfCli = self.getNtfClient()
+    result = ntfCli.removeNotificationsForUser( userName, notifsIds )
+    if not result[ 'OK' ]:
+      return S_ERROR( result[ 'Message' ] )
+    self.getStatsForUser( forceRefresh = True )
+    return S_OK()
+    
+gNotCache = NotificationsCache()
+
+class NotificationsController(BaseController):
+
+  def index(self):
+    return defaultRedirect()
+  
+  @jsonify
+  def getUserStats( self ):
+    return S_OK( gNotCache.getStatsForUser() )
+  
+  @jsonify
+  def retrieveNotifications( self ):
+    return { 'notifications' : gNotCache.getNotificationsForUser() }
+  
+  @jsonify
+  def markNotificationsRead( self ):
+    try:
+      notifs = simplejson.loads( request.params[ 'notifsIds' ] )
+      notifs = [ int( id ) for id in notifs ]
+    except Exception, e:
+      return S_ERROR( "Error getting notifications: %s" % str(e) )
+    return gNotCache.markNotificationsAsSeen( True, notifs )
+  
+  @jsonify
+  def markNotificationsUnread( self ):
+    try:
+      notifs = simplejson.loads( request.params[ 'notifsIds' ] )
+      notifs = [ int( id ) for id in notifs ]
+    except Exception, e:
+      return S_ERROR( "Error getting notifications: %s" % str(e) )
+    return gNotCache.markNotificationsAsSeen( False, notifs )
+
+  @jsonify
+  def deleteNotifications( self ):
+    try:
+      notifs = simplejson.loads( request.params[ 'notifsIds' ] )
+      notifs = [ int( id ) for id in notifs ]
+    except Exception, e:
+      return S_ERROR( "Error getting notifications: %s" % str(e) )
+    return gNotCache.deleteNotifications( notifs )
+    
+    
---
+++ dirac/public/javascripts/web/userNotifications.js
@@ -0,0 +1,352 @@
+
+var gNotificationsWindow = false;
+var gGridPanel = false;
+var gRefreshNotificationsFunction = false;
+
+function initNotificationsChecker()
+{
+	gRefreshNotificationsFunction = function(){
+		Ext.Ajax.request({
+				method : 'POST',
+				success : cbStatsReceived,
+				failure : cbMSGError,
+				url : getURL( 'getUserStats' ),
+			});
+	};
+	var msgHeartbeat = {
+			run : gRefreshNotificationsFunction,
+    		interval:300000 // 5min
+		};
+	Ext.TaskMgr.start( msgHeartbeat );
+}
+
+function getURL( action )
+{
+	var messageURL = document.location.protocol + '//' + document.location.host + gURLRoot;
+	if( gPageDescription )
+	{
+		if( gPageDescription.selectedSetup )
+			messageURL += "/" + gPageDescription.selectedSetup;
+		if( gPageDescription.userData && gPageDescription.userData.group )
+			messageURL += "/" + gPageDescription.userData.group;
+	}
+	return messageURL + '/web/notifications/' + action;
+}
+
+function cbMSGError( ajaxResult, ajaxRequest )
+{
+	//Error? server did not connect?
+}
+
+
+function cbStatsReceived( ajaxResult, ajaxRequest )
+{
+	if( ajaxResult.status != 200 )
+		return;
+	var result = Ext.util.JSON.decode( ajaxResult.responseText );
+	if( ! result.OK )
+		return;
+	var stats = result.Value;
+	var notifButton = Ext.ComponentMgr.get( 'mainNotificationStats' );
+	if( ! stats.totalNots )
+	{
+		notifButton.setText( "" );
+		notifButton.hide();
+		notifButton.purgeListeners();
+		return;
+	}
+	if( ! stats.newNots )
+	{
+		var unread = 0;
+		
+	}
+	else
+	{	
+		var lastReadId = getCookie( "lastReadNotId" );
+		if( ! lastReadId )
+			lastReadId = -1;
+		var unread = 0;
+		for( var i = 0; i< stats.newNots.length; i++  )
+		{
+			if( stats.newNots[i] > lastReadId )
+				unread += 1;
+		}
+	}
+	if( unread )
+		var text = "<font color='red'>" + unread + " unread notifications</font>";
+	else
+		var text = "" + stats.totalNots + " notifications";
+		
+	notifButton.setText( text );
+	notifButton.show();
+	if( ! notifButton.hasListener( 'click' ) )
+		notifButton.on( 'click', cbDisplayNotifications );
+}
+
+function cbDisplayNotifications()
+{
+	var gridPanel = createNotificationsGrid();
+	var topToolBar = new Ext.Toolbar({
+		region : 'north',
+		items : [ {
+		      	  	cls:"x-btn-text-icon",
+				    handler:function(){cbSelectNotifications(true)},
+				    icon:gURLRoot+'/images/iface/checked.gif',
+				    text:'Select All',
+				    tooltip:'Click to select all rows',
+		          },{
+		        	cls:"x-btn-text-icon",
+		        	handler:function(){cbSelectNotifications(false)},
+		        	icon:gURLRoot+'/images/iface/unchecked.gif',
+		        	text:'Select None',
+		        	tooltip:'Click to uncheck selected row(s)',
+		          },
+		          '->',
+		          /*
+		          new Ext.Toolbar.Button({ 
+		        	  text : 'Mark as unread', 
+		        	  listeners : { click : function(){cbSetUnreadNotifications(gridPanel)} },
+		          }),
+		          */
+		          '-',
+		          new Ext.Toolbar.Button({ 
+		        	  text : 'Delete', 
+		        	  listeners : { click : function(){cbDeleteNotifications(gridPanel)} },
+		          }),
+		]
+	});
+	gNotificationsWindow = new Ext.Window({
+	    iconCls : 'icon-grid',
+	    closable : true,
+	    autoScroll : true,
+	    width : 600,
+	    height : 350,
+	    border : true,
+	    collapsible : true,
+	    constrain : true,
+	    constrainHeader : true,
+	    maximizable : true,
+	    layout : 'border',
+	    plain : true,
+	    shim : false,
+	    title : "Notifications",
+	    items : [ topToolBar, gridPanel ]
+	  })
+	gNotificationsWindow.show();
+}
+
+function cbSelectNotifications( check )
+{
+	var inputs = document.getElementsByTagName('input');
+	for (var i = 0; i < inputs.length; i++) 
+	{
+		if (inputs[i].type && inputs[i].type == 'checkbox')
+		{
+			if( inputs[i].name.indexOf( 'notifMSGId') == 0)
+			{
+				if (check)
+					inputs[i].checked = true;
+				else
+					inputs[i].checked = false;
+			}
+		}
+	}
+}
+
+function _getSelectedNotifications()
+{
+	var notIds = [];
+	var inputs = document.getElementsByTagName('input');
+	for (var i = 0; i < inputs.length; i++) 
+	{
+		if (inputs[i].type && inputs[i].type == 'checkbox')
+		{
+			if( inputs[i].name.indexOf( 'notifMSGId') == 0 && inputs[i].checked)
+			{
+				notIds.push( inputs[i].value );
+			}
+		}
+	}
+	return notIds;
+}
+
+function cbRefreshNotifications( ajaxResult, ajaxRequest )
+{
+	if( ajaxResult.status != 200 )
+		return;
+	var result = Ext.util.JSON.decode( ajaxResult.responseText );
+	if( ! result.OK )
+		return;
+	ajaxRequest.gridPanelToRefresh.store.load();
+	gRefreshNotificationsFunction();
+}
+
+function cbSetUnreadNotifications( gridPanel )
+{
+	var notIds = _getSelectedNotifications();
+	Ext.Ajax.request({
+		method : 'POST',
+		success : cbRefreshNotifications,
+		failure : cbMSGError,
+		params : { notifsIds : Ext.util.JSON.encode( notIds ) },
+		url : getURL( 'markNotificationsUnread' ),
+		gridPanelToRefresh : gridPanel
+	});
+}
+
+function cbDeleteNotifications( gridPanel )
+{
+	var notIds = _getSelectedNotifications();
+	Ext.Ajax.request({
+		method : 'POST',
+		success : cbRefreshNotifications,
+		failure : cbMSGError,
+		params : { notifsIds : Ext.util.JSON.encode( notIds ) },
+		url : getURL( 'deleteNotifications' ),
+		gridPanelToRefresh : gridPanel
+	});
+}
+
+function gridFormatDate(date) 
+{
+    if (!date) 
+    {
+        return '';
+    }
+    var now = new Date();
+    var d = now.clearTime(true);
+    var notime = date.clearTime(true).getTime();
+    if (notime == d.getTime()) 
+    {
+        return 'Today ' + date.dateFormat('g:i a');
+    }
+    d = d.add('d', -6);
+    if (d.getTime() <= notime) 
+    {
+        return date.dateFormat('D g:i a');
+    }
+    return date.dateFormat('n/j g:i a');
+}
+
+function gridFormatMessage( msg )
+{
+	return msg.replace( /\n/g, "<br/>" );
+}
+
+function gridFormatRead( read )
+{
+	if( ! read )
+		var url = gURLRoot + "/images/monitoring/waiting.gif";
+	else
+		var url = gURLRoot + "/images/monitoring/unknown.gif";
+	return "<img src='"+url+"'/>";
+}
+
+function gridFormatId( id )
+{
+	return "<input type='checkbox' name='notifMSGId-"+id+"' value='"+id+"'/>";
+}
+
+function createNotificationsGrid()
+{
+	
+	var notGrid = new Ext.grid.GridPanel({
+		store : new Ext.data.JsonStore({
+			url : getURL( 'retrieveNotifications' ),
+			root : 'notifications',
+			fields: ['id', 'seen', 'message', {name:'timestamp', type:'date'}],
+			id : 'notificationsStore',
+		}),
+		columns : [ { id : 'id', header : 'ID', dataIndex : 'id', sortable : true, width : 10, renderer : gridFormatId },
+		            { header : 'Read', dataIndex : 'seen', sortable : true, width : 10, renderer : gridFormatRead },
+		            { header : 'Date', dataIndex : 'timestamp', sortable : true, renderer : gridFormatDate, width : 30 },
+		            { header : 'Message', dataIndex : 'message', sortable : false, renderer : gridFormatMessage }
+		          ],
+		viewConfig : {
+			forceFit : true,
+		},
+		sm: new Ext.grid.RowSelectionModel({
+            singleSelect:true
+        }),
+        region: 'center',
+        id: 'notificationsWindow',
+        loadMask: { msg: 'Loading Notifications...'  }
+		
+	});
+	notGrid.store.on( 'load', cbNotificationsReceived );
+	notGrid.store.keepUnread = true;
+	notGrid.store.load();
+	return notGrid;
+}
+
+
+function cbNotificationsReceived( store, records, options )
+{
+	var newNotifs = [];
+	var maxId = 0;
+	for( var i = 0; i < records.length; i++ )
+	{
+		if( ! records[i].data.seen )
+		{
+			var id = records[i].data.id
+			if( id > maxId )
+				maxId = id;
+			newNotifs.push( id );
+		}
+	}
+	if( newNotifs.length > 0 && ! store.keepUnread )
+	{
+		Ext.Ajax.request({
+			method : 'POST',
+			success : cbMessagesMarkedRead,
+			failure : cbMSGError,
+			params : { notifsIds : Ext.util.JSON.encode( newNotifs ) },
+			url : getURL( 'markNotificationsRead' ),
+		});
+		setCookie( 'lastReadNotId', maxId );
+	}
+	gRefreshNotificationsFunction();
+}
+
+function cbMessagesMarkedRead( ajaxResult, ajaxRequest )
+{
+	if( ajaxResult.status != 200 )
+		return;
+	var result = Ext.util.JSON.decode( ajaxResult.responseText );
+	if( ! result.OK )
+		window.alert( result.Message );
+}
+
+
+function setCookie( cookieName, value, exp_y, exp_m, exp_d, path, domain, secure )
+{
+  var cookie_string = cookieName + "=" + escape ( value );
+
+  if ( exp_y )
+  {
+    var expires = new Date ( exp_y, exp_m, exp_d );
+    cookie_string += "; expires=" + expires.toGMTString();
+  }
+
+  if ( path )
+        cookie_string += "; path=" + escape ( path );
+
+  if ( domain )
+        cookie_string += "; domain=" + escape ( domain );
+  
+  if ( secure )
+        cookie_string += "; secure";
+  
+  document.cookie = cookie_string;
+}
+
+
+function getCookie( cookieName )
+{
+  var matchResults = document.cookie.match ( '(^|;) ?' + cookieName + '=([^;]*)(;|$)' );
+  
+  if ( matchResults )
+    return ( unescape ( matchResults[2] ) );
+  else
+    return null;
+}
