d8d864f2d42ea99ffcf72e7adea0adc1367627ea
Adria Casajus <adria@ecm.ub.es>
2009-11-03 14:41:15 +0000

    1st try for multiple controllers, static files and templates
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@17999 4525493e-7705-40b1-a816-d608a930855b

--- dirac/config/environment.py
+++ dirac/config/environment.py
@@ -15,8 +15,8 @@ def load_environment(global_conf, app_conf):
     # Pylons paths
     root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     paths = dict(root=root,
-                 controllers=os.path.join(root, 'controllers'),
-                 static_files=os.path.join(root, 'public'),
+                 controllers=[ os.path.join(root, 'controllers') ],
+                 static_files=[ os.path.join(root, 'public') ],
                  templates=[os.path.join(root, 'templates')])
 
     # Initialize config with the basic options
