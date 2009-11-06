538d7d4903446cdfda34ca12b8c8fb7053ad07c6
Adria Casajus <adria@ecm.ub.es>
2009-11-06 12:22:43 +0000

    Added loading extensions <extensionroot>/Web/web.cfg will be loaded if exists <extensionroot>/Web/(controllers,templates,public) will be used if exist
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@18093 4525493e-7705-40b1-a816-d608a930855b

--- dirac/config/environment.py
+++ dirac/config/environment.py
@@ -14,16 +14,19 @@ def load_environment(global_conf, app_conf):
     """
     # Pylons paths
     root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    paths = dict(root=root,
-                 controllers=[ os.path.join(root, 'controllers') ],
-                 static_files=[ os.path.join(root, 'public') ],
-                 templates=[ os.path.join(root, 'templates') ])
+    diracConfig = initDIRAC( root )
+    paths = dict( root = root,
+                  controllers = diracConfig[ 'controllers' ],
+                  static_files = diracConfig[ 'public' ],
+                  templates = diracConfig[ 'templates' ] )
 
     # Initialize config with the basic options
     config.init_app(global_conf, app_conf, package='dirac',
                     template_engine='mako', paths=paths)
-
-    initDIRAC( config, root )
+    #Add dirac configs
+    for k in diracConfig[ 'webConfig' ]:
+      config[ k ] = diracConfig[ 'webConfig' ][ k ]
+    
 
     config['routes.map'] = make_map()
     config['pylons.g'] = app_globals.Globals()
@@ -33,12 +36,13 @@ def load_environment(global_conf, app_conf):
     tmpl_options = config['buffet.template_options']
 
 
-def initDIRAC( config, root ):
+def initDIRAC( root ):
     # CONFIGURATION OPTIONS HERE (note: all config options will override
     # any Pylons config options)
-    config['dirac.webroot'] = root
-    diracRootPath = os.path.realpath( os.path.dirname( root ) )
-    config['dirac.root'] = diracRootPath
+    configDict = { 'webConfig' : {} }
+    configDict[ 'webConfig' ]['dirac.webroot'] = root
+    diracRootPath = os.path.realpath( os.path.dirname( os.path.dirname( root ) ) )
+    configDict[ 'webConfig' ]['dirac.root'] = diracRootPath
     if diracRootPath not in sys.path:
       sys.path.append( diracRootPath )
     from DIRAC.FrameworkSystem.Client.Logger import gLogger
@@ -57,3 +61,27 @@ def initDIRAC( config, root ):
     gMonitor.registerActivity( "pagesServed", "Pages served", "Framework", "pages", gMonitor.OP_SUM )
 
     gLogger.info( "DIRAC Initialized" )
+    
+    extModules = [ '%sDIRAC' % module for module in gConfig.getValue( "/DIRAC/Extensions", [] ) ]
+    #Load web.cfg of modules
+    for extModule in extModules:
+      webCFGPath = os.path.join( diracRootPath, extModule, "Web", "web.cfg" )
+      if os.path.isfile( webCFGPath ):
+        gLogger.info( "Adding web.cfg for %s extension" % extModule )
+        gConfig.loadFile( webCFGPath )
+    #Define the controllers, templates and public directories
+    for type in ( 'controllers', 'templates', 'public' ):
+      configDict[ type ] = []
+      for extModule in extModules:
+        typePath = os.path.join( diracRootPath, extModule, "Web", type)
+        if os.path.isdir( typePath ):
+          gLogger.info( "Adding %s path for module %s" % ( type, extModule ) )
+          configDict[ type ].append( typePath )
+      #End of extensions
+      configDict[ type ].append( os.path.join(root,  type ) )
+      
+    gLogger.info( "Extension modules loaded" )
+    
+    return configDict
+    
+    
