347fd339e2d43ec9b858932ba0d84d9c2895eb0b
Adria Casajus <adria@ecm.ub.es>
2010-04-07 16:49:49 +0000

    FIX: Use new routes format ( Nice one pylons devs... :P )
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@24071 4525493e-7705-40b1-a816-d608a930855b

--- dirac/config/routing.py
+++ dirac/config/routing.py
@@ -10,60 +10,59 @@ import re
 from pylons import config
 from routes import Mapper
 
-def controller_scan(directory=None):
-    """Scan a directory for python files and use them as controllers"""
-    if directory is None:
-        return []
-    
-    def find_controllers(dirname, prefix=''):
-        """Locate controllers in a directory"""
-        controllers = []
-        if type( dirname ) not in ( types.ListType, types.TupleType ):
-          dirs = [ dirname ]
-        else:
-          dirs = dirname 
-        for dirname in dirs:
-          for fname in os.listdir(dirname):
-              filename = os.path.join(dirname, fname)
-              if os.path.isfile(filename) and \
-                  re.match('^[^_]{1,1}.*\.py$', fname):
-                  controllers.append(prefix + fname[:-3])
-              elif os.path.isdir(filename):
-                  controllers.extend(find_controllers(filename, 
-                                                      prefix=prefix+fname+'/'))
-        return controllers
-    def longest_first(fst, lst):
-        """Compare the length of one string to another, shortest goes first"""
-        return cmp(len(lst), len(fst))
-    controllers = find_controllers(directory)
-    controllers.sort(longest_first)
+def controller_scan( directory = None ):
+  """Scan a directory for python files and use them as controllers"""
+  if directory is None:
+    return []
+
+  def find_controllers( dirname, prefix = '' ):
+    """Locate controllers in a directory"""
+    controllers = []
+    if type( dirname ) not in ( types.ListType, types.TupleType ):
+      dirs = [ dirname ]
+    else:
+      dirs = dirname
+    for dirname in dirs:
+      for fname in os.listdir( dirname ):
+        filename = os.path.join( dirname, fname )
+        if os.path.isfile( filename ) and \
+          re.match( '^[^_]{1,1}.*\.py$', fname ):
+          controllers.append( prefix + fname[:-3] )
+        elif os.path.isdir( filename ):
+          controllers.extend( find_controllers( filename,
+                                                prefix = prefix + fname + '/' ) )
     return controllers
 
+  controllers = sorted( find_controllers( directory ) )
+  return controllers
+
 def make_map():
-    """Create, configure and return the routes Mapper"""
-    map = Mapper(controller_scan=controller_scan,
-                 directory=config['pylons.paths']['controllers'],
-                 always_scan=config['debug'])
 
-    # The ErrorController route (handles 404/500 error pages); it should
-    # likely stay at the top, ensuring it can always be resolved
-    map.connect('error/:action/:id', controller='error')
+  """Create, configure and return the routes Mapper"""
+  map = Mapper( controller_scan = controller_scan,
+               directory = config['pylons.paths']['controllers'],
+               always_scan = config['debug'],
+               explicit = True )
+  map.minimization = False
 
-    # CUSTOM ROUTES HERE
-    import dirac.lib.credentials as credentials
-    condDict = dict(function=credentials.checkURL)
+  # The ErrorController route (handles 404/500 error pages); it should
+  # likely stay at the top, ensuring it can always be resolved
+  map.connect( 'error/{action}', controller = 'error' )
+  map.connect( 'error/{action}/{id}', controller = 'error' )
 
-    map.connect('', controller = 'info/general' )
-    map.connect(':dsetup/:dgroup/:controller/:action/:id', conditions=condDict )
-    map.connect(':dsetup/:controller/:action/:id',         dgroup='unknown', conditions=condDict )
-    map.connect(':controller/:action/:id',                 dsetup='unknown', dgroup='unknown', conditions=condDict )
-    map.connect(':dsetup/:dgroup/:controller/:action', id=None, conditions=condDict )
-    map.connect(':dsetup/:controller/:action',         id=None, dgroup='unknown', conditions=condDict )
-    map.connect(':controller/:action',                 id=None, dsetup='unknown', dgroup='unknown', conditions=condDict )
-    map.connect(':dsetup/:dgroup/:controller', action='index', id=None, conditions=condDict )
-    map.connect(':dsetup/:controller',         action='index', id=None, dgroup='unknown', conditions=condDict )
-    map.connect(':controller',                 action='index', id=None, dsetup='unknown', dgroup='unknown', conditions=condDict )
+  # CUSTOM ROUTES HERE
+  import dirac.lib.credentials as credentials
+  condDict = { 'function' : credentials.checkURL }
+  reqs = { 'controller' : R"(\w+(?:/\w+)?)?" }
 
-    map.connect('*url', controller='template', action='view')
+  map.connect( '/', controller = 'info/general', action = "diracOverview" )
+  map.connect( '/{dsetup}/{dgroup}/{controller}/{action}', conditions = condDict, requirements = reqs )
+  map.connect( '/{dsetup}/{dgroup}/{controller}', action = 'index', conditions = condDict, requirements = reqs )
+  map.connect( '/{dsetup}/{dgroup}/{controller}/{action}/{id}', conditions = condDict, requirements = reqs )
+  map.connect( '/{controller}/{action}', dsetup = 'unknown', dgroup = 'unknown', conditions = condDict, requirements = reqs )
+  map.connect( '/{controller}/{action}/{id}', dsetup = 'unknown', dgroup = 'unknown', conditions = condDict, requirements = reqs )
+  map.connect( '/{controller}', action = 'index', dsetup = 'unknown', dgroup = 'unknown', conditions = condDict, requirements = reqs )
 
-    return map
+  map.connect( '*url', controller = 'template', action = 'view' )
+
+  return map
