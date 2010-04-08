184b8cb57c07f9bae757c540e24f2d024f96181a
Adria Casajus <adria@ecm.ub.es>
2010-04-08 11:13:20 +0000

    NEW: Added possibility to find controllers outside dirac web (for extensions)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@24087 4525493e-7705-40b1-a816-d608a930855b

--- dirac/config/middleware.py
+++ dirac/config/middleware.py
@@ -1,6 +1,6 @@
 """Pylons middleware initialization"""
 
-import types 
+import types
 from beaker.middleware import CacheMiddleware, SessionMiddleware
 from routes.middleware import RoutesMiddleware
 
@@ -14,9 +14,11 @@ from pylons.error import error_template
 from pylons.middleware import error_mapper, ErrorDocuments, ErrorHandler
 from pylons.wsgiapp import PylonsApp
 
+from dirac.lib.DiracWebApp import DiracWebApp
+
 from dirac.config.environment import load_environment
 
-def make_app(global_conf, full_stack=True, **app_conf):
+def make_app( global_conf, full_stack = True, **app_conf ):
     """Create a Pylons WSGI application and return it
 
     ``global_conf``
@@ -35,26 +37,27 @@ def make_app(global_conf, full_stack=True, **app_conf):
         defaults to main).
     """
     # Configure the Pylons environment
-    load_environment(global_conf, app_conf)
+    load_environment( global_conf, app_conf )
 
     # The Pylons WSGI app
-    app = PylonsApp()
+    # Overload with DIRAC app
+    app = DiracWebApp()
 
     # CUSTOM MIDDLEWARE HERE (filtered by error handling middlewares)
-    app = RoutesMiddleware(app, config['routes.map'])
-    app = SessionMiddleware(app, config)
-    app = CacheMiddleware(app, config)
+    app = RoutesMiddleware( app, config['routes.map'] )
+    app = SessionMiddleware( app, config )
+    app = CacheMiddleware( app, config )
 
-    if asbool(full_stack):
+    if asbool( full_stack ):
         # Handle Python exceptions
-        app = ErrorHandler(app, global_conf, **config['pylons.errorware'])
+        app = ErrorHandler( app, global_conf, **config['pylons.errorware'] )
 
         # Display error documents for 401, 403, 404 status codes (and
         # 500 when debug is disabled)
-        app = ErrorDocuments(app, global_conf, mapper=error_mapper, **app_conf)
+        app = ErrorDocuments( app, global_conf, mapper = error_mapper, **app_conf )
 
     # Establish the Registry for this application
-    app = RegistryManager(app)
+    app = RegistryManager( app )
 
     # Static files
     staticFiles = config['pylons.paths']['static_files']
---
+++ dirac/lib/DiracWebApp.py
@@ -0,0 +1,58 @@
+
+import os
+import sys
+
+from pylons.wsgiapp import PylonsApp
+from pylons.util import class_name_from_module_name
+
+from DIRAC import gConfig, rootPath, gLogger
+
+class DiracWebApp( PylonsApp ):
+
+  def __init__( self, *args, **kwargs ):
+    super( DiracWebApp, self ).__init__( *args, **kwargs )
+    #Find base modules path
+    self.__baseControllerPaths = []
+    for module in gConfig.getValue( "/DIRAC/Extensions", [] ):
+      module = "%sDIRAC" % module
+      if os.path.isdir( os.path.join( rootPath, module, "Web", "controllers" ) ):
+        self.__baseControllerPaths.append( "%s.Web.controllers" % module )
+    self.__baseControllerPaths.append( 'dirac.controllers' )
+    self.__log = gLogger.getSubLogger( "DIRACWebApp" )
+    self.__log.info( "Base modules to find controllers are:\n\t%s" % "\n\t".join ( self.__baseControllerPaths ) )
+
+
+  def find_controller( self, controller ):
+    # Check to see if we've cached the class instance for this name
+    if controller in self.controller_classes:
+      return self.controller_classes[ controller ]
+
+    self.__log.info( "Trying to find %s controller" % controller )
+    controllerModule = controller.replace( "/", "." )
+    for rootModule in self.__baseControllerPaths:
+      fullModuleName = "%s.%s" % ( rootModule, controllerModule )
+
+      # Hide the traceback here if the import fails (bad syntax and such)
+      __traceback_hide__ = 'before_and_this'
+
+      try:
+        __import__( fullModuleName )
+      except ImportError:
+        self.__log.info( "Cannot import %s" % fullModuleName )
+        continue
+      if hasattr( sys.modules[ fullModuleName ], '__controller__' ):
+        mycontroller = getattr( sys.modules[ fullModuleName ],
+                                sys.modules[ fullModuleName ].__controller__ )
+      else:
+        moduleName = controller.split( '/' )[-1]
+        className = class_name_from_module_name( moduleName ) + 'Controller'
+        self.__log.debug( "Found controller, module: '%s', class: '%s', rootModule: '%s'" % ( fullModuleName,
+                                                                                              className,
+                                                                                              rootModule ) )
+        mycontroller = getattr( sys.modules[ fullModuleName ], className )
+      self.__log.info( "Imported %s controller" % fullModuleName )
+      self.controller_classes[ controller ] = mycontroller
+      return mycontroller
+    self.__log.error( "Could not import controller %s" % controller )
+    raise Exception( "Couldn't find controller %s in root modules %s" % ( controller,
+                                                                          self.__baseControllerPaths ) )
