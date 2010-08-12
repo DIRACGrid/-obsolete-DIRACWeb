1cb7ecdd84b037210b7bc13d7a0b97076a5d0ce9
Ricardo Graciani <graciani@ecm.ub.es>
2010-08-12 16:40:50 +0000

    FIX: several typos
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@27977 4525493e-7705-40b1-a816-d608a930855b

--- dirac-postInstall.py
+++ dirac-postInstall.py
@@ -13,13 +13,13 @@ os.makedirs( dataDir, mode = 0777 )
 
 destDir = os.path.join( basedir, 'dirac', 'public' )
 for comp in ['ext', 'yui']:
-  tarName = os.path.join( basedir, 'tarballs', 'js', '%s.tar.bz' % comp )
-  if os.system( 'tar xjf %s -C %s' ( tarName, destDir ) ):
+  tarName = os.path.join( basedir, 'tarballs', 'js', '%s.tar.bz2' % comp )
+  if os.system( 'tar xjf %s -C %s' % ( tarName, destDir ) ):
     sys.exit( 1 )
 
 destDir = os.path.join( basedir, 'dirac', 'public', 'images' )
-tarName = os.path.join( basedir, 'tarballs', 'images', 'flogs.tar.gz' )
+tarName = os.path.join( basedir, 'tarballs', 'images', 'flags.tar.gz' )
 
-if os.system( 'tar xjf %s -C %s' ( tarName, destDir ) ):
+if os.system( 'tar xjf %s -C %s' % ( tarName, destDir ) ):
   sys.exit( 1 )
 
