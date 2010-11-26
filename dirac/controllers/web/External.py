7e903315186f71d5cb23b3ee50db012fe06cf1ed
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2010-11-26 15:47:04 +0000

    Default external URL is set to google.com
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@30858 4525493e-7705-40b1-a816-d608a930855b

--- dirac/controllers/web/External.py
+++ dirac/controllers/web/External.py
@@ -9,4 +9,6 @@ class ExternalController(BaseController):
     if request.params.has_key("site") and len(request.params["site"]) > 0:
       if str(request.params["site"]) != "All":
         c.select = str(request.params["site"])
+    else:
+      c.select =  gConfig.getValue( "/Website/DefaultExternalURL", "google.com" )
     return render("web/External.mako")
