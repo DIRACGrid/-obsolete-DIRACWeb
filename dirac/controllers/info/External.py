e188eb6c2472f4a4c1bbea7e6fa43a28b4a2b35c
Matvey Sapunov <Matvey.Sapunov@cern.ch>
2010-11-26 18:15:52 +0000

    Use as default index page
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/Web/trunk/Web@30860 4525493e-7705-40b1-a816-d608a930855b

--- dirac/controllers/info/External.py
+++ dirac/controllers/info/External.py
@@ -1,6 +1,7 @@
 import logging
 
 from dirac.lib.base import *
+from DIRAC.ConfigurationSystem.Client.Config import gConfig
 
 log = logging.getLogger(__name__)
 
@@ -9,4 +10,6 @@ class ExternalController(BaseController):
     if request.params.has_key("site") and len(request.params["site"]) > 0:
       if str(request.params["site"]) != "All":
         c.select = str(request.params["site"])
+    else:
+      c.select =  gConfig.getValue( "/Website/DefaultExternalURL", "google.com" )
     return render("info/External.mako")
