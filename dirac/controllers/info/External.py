8647d643971c7bdacc63984a4b74fa3196352ecc
Adrià Casajús <adria@ecm.ub.es>
2011-05-20 20:04:45 +0200

    FIX: External is properly set to diracgrid.org FIX: All references to info/External have been moved to web/External. Extensions should do the same

--- dirac/controllers/info/External.py
+++ dirac/controllers/info/External.py
@@ -6,10 +6,15 @@ from DIRAC.ConfigurationSystem.Client.Config import gConfig
 log = logging.getLogger(__name__)
 
 class ExternalController(BaseController):
+
+  def index(self):
+    return self.display()
+
   def display(self):
-    if request.params.has_key("site") and len(request.params["site"]) > 0:
-      if str(request.params["site"]) != "All":
-        c.select = str(request.params["site"])
+    if request.params.has_key( "site" ) and len( request.params[ "site" ] ) > 0:
+      if str( request.params[ "site" ] ) != "All":
+        c.select = str( request.params[ "site" ] )
     else:
-      c.select =  gConfig.getValue( "/Website/DefaultExternalURL", "google.com" )
-    return render("info/External.mako")
+      c.select =  gConfig.getValue( "/Website/DefaultExternalURL", "http://diracgrid.org" )
+    return render( "web/External.mako" )
+
