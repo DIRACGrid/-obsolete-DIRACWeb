bbed1bf8a678e741b9612e98f484b9485f41e597
Matvey Sapunov <matvey.sapunov@gmail.com>
2011-11-07 10:00:15 +0100

    console object is unknown by default in some of FF versions in linux

--- dirac/public/javascripts/web/External.js
+++ dirac/public/javascripts/web/External.js
@@ -7,7 +7,6 @@ function initExternal(reponseSelect){
 // function describing data structure, should be individual per page
 // Initialisation of selection sidebar, all changes with selection items should goes here
 function renderData(site){
-  console.log( "HERE!!" );
   var html = '<iframe id="www_frame" src =' + site + '></iframe>';
   var mainContent = new Ext.Panel({border:0,autoScroll:false,html:html,region:'center'})
   mainContent.on('resize',function(){
