a0d5e13f3608b5075743836ad6336b48357e5791
Matvey Sapunov <matvey.sapunov@gmail.com>
2011-11-14 19:44:56 +0100

    add function to send mail using NotificationService

--- dirac/controllers/info/general.py
+++ dirac/controllers/info/general.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 from dirac.lib.base import *
+from DIRAC import gConfig, gLogger
+from dirac.lib.diset import getRPCClient
+from DIRAC.FrameworkSystem.Client.NotificationClient import NotificationClient
 
 log = logging.getLogger( __name__ )
 
@@ -13,24 +16,293 @@ class GeneralController( BaseController ):
   def diracOverview( self ):
     return render( "/info/diracOverview.mako" )
 
-  def diracWhitepaper( self ):
-    return render( "/info/diracOverview.mako" )
-
-  def teamPeople( self ):
-    return render( "/info/diracOverview.mako" )
-
-  def teamRestaurants( self ):
-    return render( "/info/diracOverview.mako" )
-
-  def teamPubs( self ):
-    return render( "/info/diracOverview.mako" )
-
-  def download( self ):
-    return render( "/info/diracOverview.mako" )
-
-  def logos( self ):
-    c.imagePath = "%s/public/images/logos/" % os.path.dirname( os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) ) )
-    return render( "/info/logos.mako" )
-
   def ext4test( self ):
     return render( "/info/ext4test.mako" )
+        
+  @jsonify
+  def action(self):
+    if request.params.has_key("getVOList") and len(request.params["getVOList"]) > 0:
+      return {"success":"true","result":self.getVOList()}
+    elif request.params.has_key("getCountries") and len(request.params["getCountries"]) > 0:
+      return {"success":"true","result":self.getCountries()}
+    elif request.params.has_key("registration_request") and len(request.params["registration_request"]) > 0:
+      paramcopy = dict()
+      for i in request.params:
+        if not i == "registration_request" and len(request.params[i]) > 0:
+          paramcopy[i] = request.params[i]
+      return self.registerUser(paramcopy)
+    else:
+      return {"success":"false","error":"The request parameters can not be recognized or they are not defined"}
+
+  def registerUser(self,paramcopy):
+# Unfortunately there is no way to get rid of empty text values in JS, so i have to hardcode it on server side. Hate it!
+    default_values = ["John Smith","jsmith","john.smith@gmail.com","+33 9 10 00 10 00","Select prefered virtual organization(s)"]
+    default_values.append("Select your country")
+    default_values.append("Any additional information you want to provide to administrators")
+# Check for DN and name anonimous
+    ntc = NotificationClient( getRPCClient )
+    body = ""
+    for i in paramcopy:
+      if not paramcopy[i] in default_values:
+        body = body + str(i) + ' - ' + str(paramcopy[i]) + '\n'
+    body = str(body)
+    gLogger.info(body)
+# Check for previous requests
+    result = ntc.sendMail("matvey.sapunov@gmail.com","New user has arrived",body,"adduser@diracgrid.org",False)
+    if not result["OK"]:
+      return {"success":"false","error":result["Message"]}
+    return {"success":"true","result":result["Value"]}
+
+  def getVOList(self):
+    result = gConfig.getSections("/Registry/VO")
+    if result["OK"]:
+      vo = result["Value"]
+    else:
+      vo = ""
+    return vo
+    
+  def getCountries(self):
+    countries = {
+    "af": "Afghanistan",
+    "al": "Albania",
+    "dz": "Algeria",
+    "as": "American Samoa",
+    "ad": "Andorra",
+    "ao": "Angola",
+    "ai": "Anguilla",
+    "aq": "Antarctica",
+    "ag": "Antigua and Barbuda",
+    "ar": "Argentina",
+    "am": "Armenia",
+    "aw": "Aruba",
+    "au": "Australia",
+    "at": "Austria",
+    "az": "Azerbaijan",
+    "bs": "Bahamas",
+    "bh": "Bahrain",
+    "bd": "Bangladesh",
+    "bb": "Barbados",
+    "by": "Belarus",
+    "be": "Belgium",
+    "bz": "Belize",
+    "bj": "Benin",
+    "bm": "Bermuda",
+    "bt": "Bhutan",
+    "bo": "Bolivia",
+    "ba": "Bosnia and Herzegowina",
+    "bw": "Botswana",
+    "bv": "Bouvet Island",
+    "br": "Brazil",
+    "io": "British Indian Ocean Territory",
+    "bn": "Brunei Darussalam",
+    "bg": "Bulgaria",
+    "bf": "Burkina Faso",
+    "bi": "Burundi",
+    "kh": "Cambodia",
+    "cm": "Cameroon",
+    "ca": "Canada",
+    "cv": "Cape Verde",
+    "ky": "Cayman Islands",
+    "cf": "Central African Republic",
+    "td": "Chad",
+    "cl": "Chile",
+    "cn": "China",
+    "cx": "Christmas Island",
+    "cc": "Cocos Islands",
+    "co": "Colombia",
+    "km": "Comoros",
+    "cg": "Congo",
+    "cd": "Congo",
+    "ck": "Cook Islands",
+    "cr": "Costa Rica",
+    "ci": "Cote D'Ivoire",
+    "hr": "Croatia",
+    "cu": "Cuba",
+    "cy": "Cyprus",
+    "cz": "Czech Republic",
+    "dk": "Denmark",
+    "dj": "Djibouti",
+    "dm": "Dominica",
+    "do": "Dominican Republic",
+    "tp": "East Timor",
+    "ec": "Ecuador",
+    "eg": "Egypt",
+    "sv": "El Salvador",
+    "gq": "Equatorial Guinea",
+    "er": "Eritrea",
+    "ee": "Estonia",
+    "et": "Ethiopia",
+    "fk": "Falkland Islands",
+    "fo": "Faroe Islands",
+    "fj": "Fiji",
+    "fi": "Finland",
+    "fr": "France",
+    "fx": "France, metropolitan",
+    "gf": "French Guiana",
+    "pf": "French Polynesia",
+    "tf": "French Southern Territories",
+    "ga": "Gabon",
+    "gm": "Gambia",
+    "ge": "Georgia",
+    "de": "Germany",
+    "gh": "Ghana",
+    "gi": "Gibraltar",
+    "gr": "Greece",
+    "gl": "Greenland",
+    "gd": "Grenada",
+    "gp": "Guadeloupe",
+    "gu": "Guam",
+    "gt": "Guatemala",
+    "gn": "Guinea",
+    "gw": "Guinea-Bissau",
+    "gy": "Guyana",
+    "ht": "Haiti",
+    "hm": "Heard and Mc Donald Islands",
+    "va": "Vatican City",
+    "hn": "Honduras",
+    "hk": "Hong Kong",
+    "hu": "Hungary",
+    "is": "Iceland",
+    "in": "India",
+    "id": "Indonesia",
+    "ir": "Iran",
+    "iq": "Iraq",
+    "ie": "Ireland",
+    "il": "Israel",
+    "it": "Italy",
+    "jm": "Jamaica",
+    "jp": "Japan",
+    "jo": "Jordan",
+    "kz": "Kazakhstan",
+    "ke": "Kenya",
+    "ki": "Kiribati",
+    "kp": "Korea",
+    "kr": "Korea",
+    "kw": "Kuwait",
+    "kg": "Kyrgyzstan",
+    "la": "Lao",
+    "lv": "Latvia",
+    "lb": "Lebanon",
+    "ls": "Lesotho",
+    "lr": "Liberia",
+    "ly": "Libyan",
+    "li": "Liechtenstein",
+    "lt": "Lithuania",
+    "lu": "Luxembourg",
+    "mo": "Macau",
+    "mk": "Macedonia",
+    "mg": "Madagascar",
+    "mw": "Malawi",
+    "my": "Malaysia",
+    "mv": "Maldives",
+    "ml": "Mali",
+    "mt": "Malta",
+    "mh": "Marshall Islands",
+    "mq": "Martinique",
+    "mr": "Mauritania",
+    "mu": "Mauritius",
+    "yt": "Mayotte",
+    "mx": "Mexico",
+    "fm": "Micronesia",
+    "md": "Moldova",
+    "mc": "Monaco",
+    "mn": "Mongolia",
+    "ms": "Montserrat",
+    "ma": "Morocco",
+    "mz": "Mozambique",
+    "mm": "Myanmar",
+    "na": "Namibia",
+    "nr": "Nauru",
+    "np": "Nepal",
+    "nl": "Netherlands",
+    "an": "Netherlands Antilles",
+    "nc": "New Caledonia",
+    "nz": "New Zealand",
+    "ni": "Nicaragua",
+    "ne": "Niger",
+    "ng": "Nigeria",
+    "nu": "Niue",
+    "nf": "Norfolk Island",
+    "mp": "Northern Mariana Islands",
+    "no": "Norway",
+    "om": "Oman",
+    "pk": "Pakistan",
+    "pw": "Palau",
+    "pa": "Panama",
+    "pg": "Papua New Guinea",
+    "py": "Paraguay",
+    "pe": "Peru",
+    "ph": "Philippines",
+    "pn": "Pitcairn",
+    "pl": "Poland",
+    "pt": "Portugal",
+    "pr": "Puerto Rico",
+    "qa": "Qatar",
+    "re": "Reunion",
+    "ro": "Romania",
+    "ru": "Russia",
+    "rw": "Rwanda",
+    "kn": "Saint Kitts and Nevis",
+    "lc": "Saint Lucia",
+    "vc": "Saint Vincent and the Grenadines",
+    "ws": "Samoa",
+    "sm": "San Marino",
+    "st": "Sao Tome and Principe",
+    "sa": "Saudi Arabia",
+    "sn": "Senegal",
+    "sc": "Seychelles",
+    "sl": "Sierra Leone",
+    "sg": "Singapore",
+    "sk": "Slovakia",
+    "si": "Slovenia",
+    "sb": "Solomon Islands",
+    "so": "Somalia",
+    "za": "South Africa",
+    "gs": "South Georgia and the South Sandwich Islands",
+    "es": "Spain",
+    "lk": "Sri Lanka",
+    "sh": "St. Helena",
+    "pm": "St. Pierre and Miquelon",
+    "sd": "Sudan",
+    "sr": "Suriname",
+    "sj": "Svalbard and Jan Mayen Islands",
+    "sz": "Swaziland",
+    "se": "Sweden",
+    "ch": "Switzerland",
+    "sy": "Syrian Arab Republic",
+    "tw": "Taiwan",
+    "tj": "Tajikistan",
+    "tz": "Tanzania",
+    "th": "Thailand",
+    "tg": "Togo",
+    "tk": "Tokelau",
+    "to": "Tonga",
+    "tt": "Trinidad and Tobago",
+    "tn": "Tunisia",
+    "tr": "Turkey",
+    "tm": "Turkmenistan",
+    "tc": "Turks and Caicos Islands",
+    "tv": "Tuvalu",
+    "ug": "Uganda",
+    "ua": "Ukraine",
+    "ae": "United Arab Emirates",
+    "gb": "United Kingdom",
+    "uk": "United Kingdom",
+    "us": "United States",
+    "um": "United States Minor Outlying Islands",
+    "uy": "Uruguay",
+    "uz": "Uzbekistan",
+    "vu": "Vanuatu",
+    "ve": "Venezuela",
+    "vn": "Viet Nam",
+    "vg": "Virgin Islands (British)",
+    "vi": "Virgin Islands (U.S.)",
+    "wf": "Wallis and Futuna Islands",
+    "eh": "Western Sahara",
+    "ye": "Yemen",
+    "yu": "Yugoslavia",
+    "zm": "Zambia",
+    "zw": "Zimbabwe",
+    "su": "Soviet Union"
+    }
+    return countries
