15c12e7447abdc741f880a9a2b9383aa1eef28ab
Zoltan Mathe <zoltan.mathe@cern.ch>
2014-12-11 16:02:58 +0100

    change the window border

--- dirac/public/javascripts/baner.js
+++ dirac/public/javascripts/baner.js
@@ -3,13 +3,20 @@
   Ext.onReady(function() {
         if (document.baner) {
 
+          var html = '<!DOCTYPE html> <html> <body> <p><font color="blue">Dear ' + gPageDescription.userData.username + ',<br>' + 'This portal will be not used any more. We have a new portal which is available: </font></p> <p><font color="read"> <a href="' + document.baner
+              + '" style="color:red" >' + document.baner + '</a> </font></p></body> </html>'
           win = new Ext.Window({
                 border : true,
                 collapsible : true,
                 constrain : true,
                 constrainHeader : false,
                 closable : true,
-                height : 100,
+                height : 130,
+                border : true,
+                style : {
+                  borderColor : 'red',
+                  borderStyle : 'solid'
+                },
                 layout : 'fit',
                 maximizable : false,
                 minHeight : 400,
@@ -18,10 +25,10 @@
                 plain : false,
                 shim : false,
                 width : 400,
-                title: "New web portal is available!!!!",
-                html : '<!DOCTYPE html> <html> <body> <p><font color="blue">Dear user, This portal will be not used any more. We have a new portal which is available:</font></p></body> </html>"'
+                title : "New web portal is available!!!!",
+                html : html
               });
-        win.show();
+          win.show();
         }
 
       });
